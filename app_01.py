f1399b22130c945fa97f237c1b3d2c612998dc40
Vladislav_Grudzinsky <vladik19780131@yandex.ru>
2024-08-23 01:45:43 +0300

    Homework 1. Знакомство с Flask. Initial Commit

--- app_01.py
+++ app_01.py
@@ -4,18 +4,24 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hi!'
-
-@app.route('/main/')
 def main():
     context = {'title': 'Главная'}
     return render_template('main.html', **context)
 
-@app.route('/data/')
-def data():
-    context = {'title': 'База статей'}
-    return render_template('data.html', **context)
+@app.route('/clothes/')
+def clothes():
+    context = {'title': 'Одежда'}
+    return render_template('clothes.html', **context)
+
+@app.route('/shoes/')
+def shoes():
+    context = {'title': 'Обувь'}
+    return render_template('shoes.html', **context)
+
+@app.route('/jackets/')
+def jackets():
+    context = {'title': 'Куртки'}
+    return render_template('jackets.html', **context)
 
 
 if __name__ == '__main__':
